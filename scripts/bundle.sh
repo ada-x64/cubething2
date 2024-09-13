ff58ee5bd63efd5f50ea6f9de9214e5f303f191b
ada mandala <ada.mandala@pm.me>
2024-09-12 21:39:41 -0500

    update scripts and add recc'd extensions

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -6,4 +6,16 @@ function say_and_do {
     echo -e "$GREY>$1$RESET"
     sh -c "$1"
 }
-say_and_do "bun build src/client/app.ts --outdir www/js --sourcemap=inline --minify --define 'BUILD_INFO={\"PROD\": \"$NODE_ENV\", \"HOT\": \"$HOT\"}'"
+say_and_do "$(
+cat <<EOF
+bun build src/client/app.ts \
+--outdir www/js \
+--sourcemap=inline \
+--minify \
+--define \
+'BUILD_INFO={\
+"PROD": "$NODE_ENV", \
+"HOT": "$HOT"\
+}'
+EOF
+)"
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,5 +5,10 @@ function say_and_do {
     echo -e "$GREY>$1$RESET"
     sh -c "$1"
 }
-say_and_do 'bun --hot ./src/server/index.ts | tee server.log &'
+say_and_do "$(
+cat <<EOF
+    bun --hot ./src/server/index.ts \
+    | tee server.log &
+EOF
+)"
 say_and_do 'bun scripts/watcher.ts'
